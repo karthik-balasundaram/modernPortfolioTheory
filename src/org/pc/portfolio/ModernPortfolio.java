c7898dd8283647835f1029722e6f0a1c4f752efa
karthik-uci <mitkarthik@gmail.com>
2015-09-20 23:04:59 -0700

    updated with comments

--- src/org/pc/portfolio/ModernPortfolio.java
+++ src/org/pc/portfolio/ModernPortfolio.java
@@ -7,36 +7,45 @@ public class ModernPortfolio {
 	Random random = new Random();
 	
 	private MonteCarloSimulation monteCarloSimulation;
-	private double initialInvestment;
-	private double investment;
-	private double inflationAdjustedRate = 1.0;
+	private double initialInvestment; //this is the initial investment amount
+	private double investment; // this is the latest investment/portfolioTotal after computeReturnInNextXYears() has been called for n number of times.
+	private double inflationAdjustedRate = 1.0; //inflation adjusted rate is updated with the given inlation rate for every call of computeReturnInNextXYears() 
 	private double inflationRate = 0.035;
 
 	private int iterations = 0;
 	
+	//Constructor to be called if numYears used to compute historical risk/return  is given
 	public ModernPortfolio(double initialInvestment, double inflationRate, double _return, double _risk, int numYears){
 		this(initialInvestment, inflationRate);
 		this.setMonteCarloSimulation(new MonteCarloSimulation(_return, _risk, numYears));
 	}
 	
+	//Constructor to be called if numYears used to compute historical risk/return is not given
 	public ModernPortfolio(double initialInvestment, double inflationRate, double _return, double _risk){
 		this(initialInvestment, inflationRate);
 		this.setMonteCarloSimulation(new MonteCarloSimulation(_return, _risk) );
 	}
 	
+	//private constructor used by the above public constructors
 	private ModernPortfolio(double initialInvestment, double inflationRate){
 		this.setInitialInvestment(initialInvestment);
 		this.setInvestment(initialInvestment);
 		this.setInflationRate(inflationRate);		
 	}	
 	
+	/* This method updates value for investment and inflationAdjustedRate after x years.
+	*  Calling inflationAdjustedRate(10) would update investment and inflationAdjustedRate after 10 years.
+	*  inflationAdjustedRate() can now be called with argument x, 
+	*  will result in updating investment and inflationAdjustedRate after x years from the result of previous call inflationAdjustedRate()
+	*/
 	public void computeReturnInNextXYears(int x){
 		for(int i=0;i<x;i++){
+			//Update inflationAdjustedRate for each iteration/year
 			setInflationAdjustedRate(getInflationAdjustedRate() + getInflationAdjustedRate()*inflationRate);
 			
-			//Compute return for this year
+			//Compute return percent for this year
 			double currentReturnPercent = this.getMonteCarloSimulation().nextValue();
-			//System.out.println(currentReturnPercent);
+			
 			//compute return on investment using the currentReturnPercent and add it to portfolio Total
 			setInvestment(getInvestment() + (getInvestment() * currentReturnPercent) );
 			
@@ -45,6 +54,9 @@ public class ModernPortfolio {
 		}
 	}
 	
+	/* Calling reset will set investment, inflationAdjustedRate and iterations to their initial values
+	*  that was set during object initialization.
+	*/
 	public void reset(){
 		this.setInvestment(initialInvestment);
 		this.setInflationAdjustedRate(1.0);
@@ -52,58 +64,93 @@ public class ModernPortfolio {
 		monteCarloSimulation.reset();
 	}
 	
+	/* This is the inflationAdjusted portfolio Total that is computed using the portfolio total and the inflationAdjustedRate after x iterations/years. 
+	*/
 	public double getInflationAdjustedPortfolioTotal() {
 		return getInvestment()/getInflationAdjustedRate();
 	}
 	
-	
+	/**
+ 	* @return investment
+ 	*/
 	public double getInvestment() {
 		return investment;
 	}
+	/**
+ 	* @param investment investment to set
+ 	*/
 	private void setInvestment(double investment) {
 		this.investment = investment;
 	}
-	
+	/**
+ 	* @return initialInvestment
+ 	*/
 	public double getInitialInvestment() {
 		return initialInvestment;
 	}
+	/**
+ 	* @param initialInvestment initialInvestment to set
+ 	*/
 	private void setInitialInvestment(double initialInvestment) {
 		this.initialInvestment = initialInvestment;
 	}
+	/**
+ 	* @return inflationAdjustedRate
+ 	*/
 	public double getInflationAdjustedRate() {
 		return inflationAdjustedRate;
 	}
+	/**
+ 	* @param inflationAdjustedRate inflationAdjustedRate to set
+ 	*/
 	private void setInflationAdjustedRate(double inflationAdjustedRate) {
 		this.inflationAdjustedRate = inflationAdjustedRate;
 	}
+	/**
+ 	* @return inflationRate
+ 	*/
 	public double getInflationRate() {
 		return inflationRate;
 	}
+	/**
+ 	* @param inflationRate inflationRate to set
+ 	*/
 	private void setInflationRate(double inflationRate) {
 		this.inflationRate = inflationRate;
 	}
-
-
+	/**
+ 	* @return iterations
+ 	*/
 	public int getIterations() {
 		return iterations;
 	}
-
+	/**
+ 	* @param iterations iterations to set
+ 	*/
 	private void setIterations(int iterations) {
 		this.iterations = iterations;
 	}
-
+	/**
+ 	* @return monteCarloSimulation
+ 	*/
 	public MonteCarloSimulation getMonteCarloSimulation() {
 		return monteCarloSimulation;
 	}
-
+	/**
+ 	* @param monteCarloSimulation monteCarloSimulation to set
+ 	*/
 	private void setMonteCarloSimulation(MonteCarloSimulation monteCarloSimulation) {
 		this.monteCarloSimulation = monteCarloSimulation;
 	}
-
+	/**
+ 	* @return return
+ 	*/
 	public double getReturn(){
 		return monteCarloSimulation.getMean();		
 	}
-	
+	/**
+ 	* @return risk
+ 	*/
 	public double getRisk(){
 		return monteCarloSimulation.getSD();		
 	}	
